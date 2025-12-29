24f8810a780b5e2f52dd9d3b77f4bd86b63608ad
mrgn <19243003-m0rg4n@users.noreply.gitlab.com>
2025-12-29 10:15:12 +0400

    ♻️ Refactor
    ♻️ Refactor

--- script.js
+++ script.js
@@ -7,10 +7,35 @@ canvas.height = SIZE
 
 const ctx = canvas.getContext('2d')
 
-function format({ x, y }) {
-	return {
-		x: ((x + 1) / 2) * SIZE,
-		y: (1 - (y + 1) / 2) * SIZE
+function carry(fn) {
+	return a => b => fn(b, a)
+}
+
+function pipe(...fns) {
+	return arg => fns.reduce((acc, fn) => fn(acc), arg)
+}
+
+class Draw {
+	static clear() {
+		ctx.fillStyle = BG
+		ctx.fillRect(0, 0, SIZE, SIZE)
+	}
+
+	static point({ x, y }, color = FG) {
+		const size = 10
+		ctx.fillStyle = color
+		ctx.fillRect(x - size / 2, y - size / 2, size, size)
+	}
+
+	static line(p1, p2, color = FG) {
+		ctx.strokeStyle = color
+		ctx.lineWidth = 5
+
+		ctx.beginPath()
+		ctx.moveTo(p1.x, p1.y)
+		ctx.lineTo(p2.x, p2.y)
+		ctx.stroke()
+		ctx.closePath()
 	}
 }
 
@@ -18,28 +43,7 @@ function project({ x, y, z }) {
 	return { x: x / z, y: y / z }
 }
 
-function clear() {
-	ctx.fillStyle = BG
-	ctx.fillRect(0, 0, SIZE, SIZE)
-}
-
-function point({ x, y }, size = 10) {
-	ctx.fillStyle = FG
-	ctx.fillRect(x - size / 2, y - size / 2, size, size)
-}
-
-function line(p1, p2, color = FG) {
-	ctx.strokeStyle = color
-	ctx.lineWidth = 5
-
-	ctx.beginPath()
-	ctx.moveTo(p1.x, p1.y)
-	ctx.lineTo(p2.x, p2.y)
-	ctx.stroke()
-	ctx.closePath()
-}
-
-function rotateXZ({ x, y, z }, angle) {
+function rotate({ x, y, z }, angle) {
 	// x′ = xcosθ − ysinθ
 	// y′ = xsinθ + ycosθ
 
@@ -59,60 +63,50 @@ function translateZ({ x, y, z }, dz) {
 	return { x, y, z: z + dz }
 }
 
-const vertexes = [
-	{ x: 0, y: 0.125, z: 0 },
-	{ x: 0, y: -0.125, z: -0.125 },
-	{ x: -0.125, y: -0.125, z: 0.125 },
-	{ x: 0.125, y: -0.125, z: 0.125 },
-
-	// cube
-	{ x: 0.25, y: 0.25, z: 0.25 },
-	{ x: -0.25, y: 0.25, z: 0.25 },
-	{ x: -0.25, y: -0.25, z: 0.25 },
-	{ x: 0.25, y: -0.25, z: 0.25 },
-
-	{ x: 0.25, y: 0.25, z: -0.25 },
-	{ x: -0.25, y: 0.25, z: -0.25 },
-	{ x: -0.25, y: -0.25, z: -0.25 },
-	{ x: 0.25, y: -0.25, z: -0.25 }
-]
-
-// indexes of vertexes to join
-const faces = [
-	[0, 1],
-	[0, 2],
-	[0, 3],
-	[1, 2, 3],
-
-	// cube
-	[4, 5, 6, 7],
-	[8, 9, 10, 11],
-	[4, 8],
-	[5, 9],
-	[6, 10],
-	[7, 11]
-]
+function format({ x, y }) {
+	return {
+		x: ((x + 1) / 2) * SIZE,
+		y: (1 - (y + 1) / 2) * SIZE
+	}
+}
 
 let dt = 1 / 180
 let dz = 0.5
 let angle = 0
 
 function draw() {
-	clear()
+	Draw.clear()
 
 	if (dz < 1) dz += 1 * dt
 	angle += Math.PI * dt
 
-	for (const v of vertexes) {
-		point(format(project(translateZ(rotateXZ(v, angle), dz))))
+	const getPoint = pipe(carry(rotate)(angle), carry(translateZ)(dz), project)
+
+	/* Cube (objects/cube.js) */
+
+	// for (const cv of cubeVertexes) {
+	// 	const p = format(getPoint(cv))
+	// 	Draw.point(p)
+	// }
+	for (const cf of cubeFaces) {
+		for (let i = 0; i < cf.length; i++) {
+			const p1 = getPoint(cubeVertexes[cf[i]])
+			const p2 = getPoint(cubeVertexes[cf[(i + 1) % cf.length]])
+			Draw.line(format(p1), format(p2))
+		}
 	}
 
-	for (const f of faces) {
-		for (let i = 0; i < f.length; i++) {
-			const p1 = format(project(translateZ(rotateXZ(vertexes[f[i]], angle), dz)))
-			const p2 = format(project(translateZ(rotateXZ(vertexes[f[(i + 1) % f.length]], angle), dz)))
+	/* Pyramid (objects/pyramid.js) */
 
-			line(p1, p2)
+	// for (const cv of pyramidVertexes) {
+	// 	const p = format(getPoint(cv))
+	// 	Draw.point(p, pyramidColor)
+	// }
+	for (const pf of pyramidFaces) {
+		for (let i = 0; i < pf.length; i++) {
+			const p1 = getPoint(pyramidVertexes[pf[i]])
+			const p2 = getPoint(pyramidVertexes[pf[(i + 1) % pf.length]])
+			Draw.line(format(p1), format(p2), pyramidColor)
 		}
 	}
 
